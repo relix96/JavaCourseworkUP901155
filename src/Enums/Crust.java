252d2c4935644df0b6a18e5b6a86904273b4e557
Zé Maria Valentim <zemariavalentim@hotmail.com>
2019-03-25 19:43:54 +0000

    Task 1 and task 2 done

---
+++ src/Enums/Crust.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+/**
+ *
+ * @author Zé Maria Valentim
+ */
+public enum Crust {
+    THIN(1.08),DEEP(1.10), STUFFED(2.14);
+    
+    private double crust;
+
+    Crust(double crust){
+        this.crust = crust;
+    }  
+    
+    public double getCrust(){
+        return this.crust;
+    }
+    
+    public String getInfo(){
+        return String.valueOf(crust);
+    }
+    
+}
